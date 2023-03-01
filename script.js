6778f774d5edd7f4871441e170c76c549f5a1ed1
francoromeo <francoromeo5012@gmail.com>
2023-02-28 21:58:25 -0300

    push

---
+++ script.js
@@ -0,0 +1,24 @@
+const menu = document.getElementById('icono')
+
+const enlaces = document.querySelector('.enlaces')
+menu.addEventListener("click", e=>{
+    e.preventDefault()
+    enlaces.classList.toggle('dos')
+})
+/*
+let boton = document.getElementById("icono");
+let enlaces = document.getElementById("enlaces");
+let contador = 0;
+
+boton.addEventListener("click", function(e){
+    e.preventDefault();
+    if(contador ==0){
+        enlaces.className = ("enlaces dos")
+        contador = 1;
+    } else {
+        enlaces.classList.remove("dos")
+        enlaces.className = ("enlaces uno")
+        contador = 0;
+    }
+})
+*/
